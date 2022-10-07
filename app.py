2525643034a83ea0160b6a959269fac112967c53
Shubhda Datta <106992849+shubhda18@users.noreply.github.com>
2022-10-07 10:14:46 +0000

    updated database and app

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 
 app = Flask(__name__)
-db_jobs=load_jobs_from_db()
+
 @app.route("/")
 
 def hello_jovian():
-  
+  db_jobs=load_jobs_from_db()
   return render_template('home.html', 
                            jobs=db_jobs, 
                            company_name='Jovian')
 
 @app.route("/api/jobs")
 def list_jobs():
+  db_jobs=load_jobs_from_db()
   return jsonify(db_jobs)
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+  
+@app.route("/jobs/<id>")
+def load_job(id):
+  job =load_job_from_db(id)
+  return jsonify(job)
+  
+if __name__ == '__main__:':
+  app.run(host='0.0.0.0', debug=False)
--- database.py
+++ database.py
@@ -14,4 +14,15 @@ def load_jobs_from_db():
       jobs.append(dict(row))
   return jobs
 
+  
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    
+    result = conn.execute(text("SELECT * FROM jobs where id =:val"),val=id)
+    row=result.all()
+    if len(row)==0:
+      return None
+    else:
+      return dict(row[0])
+
  
