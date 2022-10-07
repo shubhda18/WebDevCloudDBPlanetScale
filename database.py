bbe5d31df920e5279dd7b8377c33c9b0e6308f29
Shubhda Datta <106992849+shubhda18@users.noreply.github.com>
2022-10-07 08:35:43 +0000

    updated requirements.txt

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine,text
 import os
 db_conn_string = os.environ['DB_CONN_STR']
 
-engine = engine = create_engine(db_conn_string,connect_args ={
+engine = create_engine(db_conn_string,connect_args ={
   'ssl':{"ssl_ca": "/etc/ssl/cert.pem"}}    )      
 
 def load_jobs_from_db():
